4f58a99d61a74cc1065d4ea101bd35c406c0b9b6
vipulgoel30 <vipul1234goyal@gmail.com>
2024-03-04 22:03:39 +0530

    Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,6 @@ import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-      {/* <SideNavBar
-        links={[
-          {
-            url: "/api",
-            name: "Track Submission",
-            icon: Logo,
-            alt: "logo",
-          },
-          {
-            url: "/1",
-            name: "Upload Journals",
-            icon: Logo,
-            alt: "logo",
-          },
-          {
-            url: "/2",
-            name: "Approved Journals",
-            icon: Logo,
-            alt: "logo",
-          },
-          {
-            url: "/3",
-            name: "Api",
-            icon: Logo,
-            alt: "logo",
-          },
-        ]}
-      />
-      <main className=""></main>
-      <Header /> */}
-      {/* <BtnPrimary>Login</BtnPrimary> */}
       <Routes>
         <Route path="/" element={<Landing />} />
       </Routes>
