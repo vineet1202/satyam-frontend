3ec47370de62ea2c5ce42623bd91c400b9933e5c
Vipul Goel <96120213+vipulgoel30@users.noreply.github.com>
2024-03-27 13:48:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import SideNavBar from "./Components/SideBar";
 import Header from "./Components/Header";
 import Landing from "./Pages/Landing/Landing";
 import Login from "./Pages/Login/Login";
-import Landing from "./Pages/Home/Landing";
 
 import { Routes, Route } from "react-router-dom";
 import Satyam from "./Pages/Satyam/Satyam";
