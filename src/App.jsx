738e6bd56c847ab4c506ed853e890a0db0cf3d05
Vipul Goel <96120213+vipulgoel30@users.noreply.github.com>
2024-03-28 13:39:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import SideNavBar from "./Components/SideBar";
 import Header from "./Components/Header";
-import Landing from "./Pages/Landing/Landing";
-import Login from "./Pages/Login/Login";
+import Landing from "./Pages/Home/Landing";
 
 import { Routes, Route } from "react-router-dom";
 import Satyam from "./Pages/Satyam/Satyam";
@@ -9,9 +8,7 @@ import Satyam from "./Pages/Satyam/Satyam";
 const App = () => {
   return (
     <>
-    {/*<Login/>*/}
       <Routes>
-        <Route path="/" element={<Landing />} />
         <Route path="/*" element={<Landing />} />
         <Route path="/satyam/*" element={<Satyam />} />
       </Routes>
