0d3862d4d72f24c93adfd43aa2f1ec762f702e88
Vipul Goel <96120213+vipulgoel30@users.noreply.github.com>
2024-03-29 09:59:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 
@@ -16,7 +15,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Provider store={store}>
         <BrowserRouter>
           <App />
-          <ReactQueryDevtools />
         </BrowserRouter>
       </Provider>
     </QueryClientProvider>
