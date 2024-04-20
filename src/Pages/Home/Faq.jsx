e2d1fa464049196c9d7949138472ebdcce77a2c3
Yuvraj Gupta <96349681+yuvrajthegupta@users.noreply.github.com>
2024-04-20 19:05:46 +0530

    first (#12)

--- src/Pages/Home/Faq.jsx
+++ src/Pages/Home/Faq.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import "../../app.css";
-const Faq = (question, answer) => {
-  return (<>
-  <div class="container">
-  <h1>Frequently Asked Questions</h1>
-  <details>
-  <summary>How do I create accordion?</summary>
-  <div>
- The tags <em>details</em> and <em>summary</em> have you covered.
-  </div>
-</details>
-</div>
-  
-  
-  
-  </>);
-};
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import { FlexCol } from "../../Elements/Flex";
 
-export default Faq;
+const Faq = () => {
+  const [open, setOpen] = React.useState(1);
+ 
+  const handleOpen = (value) => setOpen(open === value ? 0 : value);  
+  return (
+    
+    <div>
+    <FlexCol className="mt-12 items-center">
+    <h1 className="mb-6 text-6xl font-semibold">FAQ</h1>
+    <Accordion open={open === 1}>
+    <AccordionHeader onClick={() => handleOpen(1)}>What is Material Tailwind?</AccordionHeader>
+    <AccordionBody>
+      We&apos;re not always in the position that we want to be at. We&apos;re constantly
+      growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
+      ourselves and actualize our dreams.
+    </AccordionBody>
+  </Accordion>
+  </FlexCol>
+    </div>
+  )
+}
+
+export default Faq
