ee304c7a4e6d772e3086f725b0b03d0707c03992
vineet1202 <vineetloktak@gmail.com>
2024-04-30 17:02:58 +0530

    file changed

--- src/Pages/Author/Journals.jsx
+++ src/Pages/Author/Journals.jsx
@@ -38,60 +38,64 @@ const Journals = () => {
   return (
     <>
       <div className="max-w-[1240px] mx-auto mt-12 ">
-        <h1 className="text-2xl font-bold">Journals</h1>
-        {/* {data.map((journal) => ( */}
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
-          <table class="w-full text text-left rtl:text-right ">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-200 ">
-              <tr>
-                <th scope="col" className="px-6 py-3">
-                  Journal Title
-                </th>
+        <h1 className="text-2xl font-bold text-center">Journals</h1>
 
-                <th scope="col" className="px-6 py-3">
-                  Keywords
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  PDF Link
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Reviewers
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Journal Status
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  <span className="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((journal) => (
-                <tr className="bg-white border-b  hover:bg-gray-50 ">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                  >
-                    {journal.title}
+        {data.length > 0 ? (
+          <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
+            <table class="w-full text text-left rtl:text-right ">
+              <thead class="text-xs text-gray-700 uppercase bg-gray-200 ">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    Journal Title
                   </th>
 
-                  <td className="px-6 py-4">{journal.keywords}</td>
-                  <td className="px-6 py-4">$2999</td>
-                  <td className="px-6 py-4">John</td>
-                  <td className="px-6 py-4">{journal.status}</td>
-                  <td className="px-6 py-4 text-right">
-                    <Link
-                      to={`${journal.journal_id}`}
-                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      View
-                    </Link>
-                  </td>
+                  <th scope="col" className="px-6 py-3">
+                    Keywords
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    PDF Link
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Reviewers
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Journal Status
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    <span className="sr-only">Edit</span>
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {data.map((journal) => (
+                  <tr className="bg-white border-b  hover:bg-gray-50 ">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      {journal.title}
+                    </th>
+
+                    <td className="px-6 py-4">{journal.keywords}</td>
+                    <td className="px-6 py-4">$2999</td>
+                    <td className="px-6 py-4">John</td>
+                    <td className="px-6 py-4">{journal.status}</td>
+                    <td className="px-6 py-4 text-right">
+                      <Link
+                        to={`${journal.journal_id}`}
+                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      >
+                        View
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <h1 className="text-3xl text-center mt-8">No Journals Found</h1>
+        )}
       </div>
     </>
   );
